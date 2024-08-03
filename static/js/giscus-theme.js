bcfa1a6f00e240ea31037104c9436acef9c9b1d0
picccard <7443949+picccard@users.noreply.github.com>
2024-08-03 18:08:03 +0200

    added script for changing theme on comment section

---
+++ static/js/giscus-theme.js
@@ -0,0 +1,20 @@
+function changeGiscusTheme() {
+    const theme = localStorage.getItem(THEME_PREF_STORAGE_KEY) ||
+        (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+
+    function sendMessage(message) {
+        const iframe = document.querySelector('iframe.giscus-frame');
+        if (!iframe) return;
+        iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
+    }
+    sendMessage({
+        setConfig: {
+            theme: theme,
+        },
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const darkThemeToggles = document.querySelectorAll('.dark-theme-toggle');
+    darkThemeToggles.forEach(el => el.addEventListener('click', changeGiscusTheme))
+});
